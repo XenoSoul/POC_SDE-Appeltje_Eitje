52aee89f64148b1dbd2d6a309aa64245e9973d0c
XenoSoul <yoeriwalter@gmail.com>
2021-01-16 00:01:00 +0100

    LogintTest PoC

---
+++ PoC-TestLogin/WebApp1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp1.Data;
+
+[assembly: HostingStartup(typeof(WebApp1.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<plsContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("plsContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<plsContext>();
+            });
+        }
+    }
+}
